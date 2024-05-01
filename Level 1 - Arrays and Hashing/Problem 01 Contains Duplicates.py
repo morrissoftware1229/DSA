c83837f48ba7c5eca37ff4ca4a5440115253b2b7
Charles Morris <morrissoftware1229@gmail.com>
2024-05-01 11:43:12 -0500

    Added new solution for Leetcode problem 217, Contains Duplicates using a dictionary

--- Level 1 - Arrays and Hashing/Problem 01 Contains Duplicates.py
+++ Level 1 - Arrays and Hashing/Problem 01 Contains Duplicates.py
@@ -17,18 +17,34 @@ class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         # Naive approach: Iterate through list - O(n^2)
         # The count function is O(n), and we are doing that for up to n items
+        # This approach failed a latter test for being too slow on an array with 54,500 members
+        # for num in nums:
+        #     if nums.count(num) > 1:
+        #         return True
+        #     else:
+        #         continue
+        #     return False
+
+        # Second approach: Load values into a dictionary as keys, values are counts, if value
+        #   is found in dictionary, return True
+        # This should be linear - O(n)
+        # Beats 11.47% of Python3 users on time, 11.08% on space
+        storageDict = {}
         for num in nums:
-            if nums.count(num) > 1:
+            if num in storageDict:
                 return True
             else:
-                pass
-            return False
+                storageDict[num] = 1
+        return False
+        
+Solution.containsDuplicate(Solution, nums = [2, 14, 18, 22, 22])
 
 class TestAnswer(unittest.TestCase):
     def test_contains_duplicates(self):
         self.assertTrue(Solution.containsDuplicate(Solution, nums = [1, 2, 3, 1]))
         self.assertFalse(Solution.containsDuplicate(Solution, nums = [1, 2, 3, 4]))
         self.assertTrue(Solution.containsDuplicate(Solution, nums = [1, 1, 1, 3, 3, 4, 3, 2, 4, 2]))
+        self.assertTrue(Solution.containsDuplicate(Solution, nums = [2, 14, 18, 22, 22]))
 
 unittest.main()
 
@@ -37,3 +53,5 @@ unittest.main()
 # 2. Import List to use that functionality
 # 3. Double-check the expected output and input, I had originally reversed True and False here
 # 4. Learned to run unit tests in Python3
+# 5. Difference between 'pass' and 'continue'
+# 6. Using a dictionary doesn't automatically beat 50% of users in your language
--- README.md
+++ README.md
@@ -5,4 +5,6 @@ Showcases my work on learning data structures and algorithms
 I will be using the roadmap provided on neetcode.io/roadmap to work through some simple data structures and algorithms. While I took an algorithms course during my master's, it focused on proving the time complexity and correctness of algorithms, not the implementation of and operations you would perform on certain data structures. The class also covered common algorithms like breadth-first search and depth-first search, but the focus was on using them in pseudocode as part of proofs, not implementing them in actual code to solve hypothetical problems.
 
 ## Progress
-20240501 - Completed naive approach of O(n^2) on Level 1 - Problem 01 (Contains Duplicates) and learned how to perform unit tests in Python
+###### 20240501
+- Completed naive approach of O(n^2) on Level 1 - Problem 01 (Contains Duplicates) and learned how to perform unit tests in Python
+- Completed solution to same problem using dictionary, but that only beats 11% of Python3 users
