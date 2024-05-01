7a087308f53cac65527657b02295f59d615f14ab
Charles Morris <morrissoftware1229@gmail.com>
2024-05-01 10:06:01 -0500

    Learned unit testing in Python3 and created naive solution for Leetcode problem 217, Contains Duplicates

---
+++ Level 1 - Arrays and Hashing/Problem 01 Contains Duplicates.py
@@ -0,0 +1,39 @@
+'''
+Problem 217 on Leetcode
+Rating: Easy
+
+The prompt:
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+Example input: nums = [1, 2, 3, 1]
+Expected output: true
+'''
+
+from typing import List
+import unittest
+
+# These first two lines given by Leetcode
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        # Naive approach: Iterate through list - O(n^2)
+        # The count function is O(n), and we are doing that for up to n items
+        for num in nums:
+            if nums.count(num) > 1:
+                return True
+            else:
+                pass
+            return False
+
+class TestAnswer(unittest.TestCase):
+    def test_contains_duplicates(self):
+        self.assertTrue(Solution.containsDuplicate(Solution, nums = [1, 2, 3, 1]))
+        self.assertFalse(Solution.containsDuplicate(Solution, nums = [1, 2, 3, 4]))
+        self.assertTrue(Solution.containsDuplicate(Solution, nums = [1, 1, 1, 3, 3, 4, 3, 2, 4, 2]))
+
+unittest.main()
+
+# What did I learn here?
+# 1. Pass in the class as a required argument when calling a method
+# 2. Import List to use that functionality
+# 3. Double-check the expected output and input, I had originally reversed True and False here
+# 4. Learned to run unit tests in Python3
